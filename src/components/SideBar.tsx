e1f4f88ba623a980d9c4848f74b07126dafc4007
Danielle Ismerim <danismerim@gmail.com>
2021-05-16 14:45:19 +0200

    Ignite: Desafio 02 - Componentizando a aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number
+  setSelectedGenreId: Function
+}
+
+export function SideBar(props: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.setSelectedGenreId(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
